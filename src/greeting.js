2076e202f596206990abafd7e8f203020741cb3e
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-11-22 21:56:04 -0300

    sigue nico

--- src/greeting.js
+++ src/greeting.js
@@ -1,29 +1,47 @@
 module.exports = {
-    getScore,
-    wonPoint
-}
-
-const score = ['love','15','30','40']
-let scorePlayerOne = 0
-let scorePlayerTwo = 1
-
-const playerOne = "player1"
-const playerTwo = "player2"
-
-function getScore () {
-if(scorePlayerOne==scorePlayerTwo && scorePlayerOne===3){
-    return 'Deuce';
-} else if(scorePlayerOne==scorePlayerTwo){
-    return `${score[scorePlayerOne]}-all`
-}else{
-    return `${score[scorePlayerOne]}-${score[scorePlayerTwo]}`
-}
+  getScore,
+  wonPoint,
 };
 
-function wonPoint (player) {
-    if(player == "player1"){
-        scorePlayerOne++
+const score = ["love", "15", "30", "40"];
+let advOne = false;
+let advTwo = false;
+let winner 
+let scorePlayerOne = 3;
+let scorePlayerTwo = 3;
+
+const playerOne = "PlayerOne";
+const playerTwo = "PlayerTwo";
+
+function getScore() {
+  if (advOne == true) {
+    return "Adv-1";
+  } else if (advTwo == true) {
+    return "Adv-2";
+  } else if (scorePlayerOne == scorePlayerTwo && scorePlayerOne === 3) {
+    return "Deuce";
+  } else if (scorePlayerOne == scorePlayerTwo) {
+    return `${score[scorePlayerOne]}-all`;
+  } else {
+    return `${score[scorePlayerOne]}-${score[scorePlayerTwo]}`;
+  }
+}
+
+function wonPoint(player) {
+  if (getScore() == "Deuce") {
+    if (player == "playerOne") {
+      advOne = true;
     } else {
-        scorePlayerTwo++
+      advTwo = true;
     }
+  }
+  if (getScore() == "Adv-1"){
+    playerOne
+  }
+
+  if (player == "playerOne") {
+    scorePlayerOne++;
+  } else {
+    scorePlayerTwo++;
+  }
 }
--- src/greeting.test.js
+++ src/greeting.test.js
@@ -5,9 +5,9 @@ test('0 a 1 expect love-15', () => {
     expect(greeting.getScore()).toBe("love-15");
 });
 
-test('add one point to player2',() =>{
-    greeting.wonPoint("player1")
-    expect(greeting.getScore()).toBe("15-all");
+test('player get Adv',() =>{
+    greeting.wonPoint("playerOne")
+    expect(greeting.getScore()).toBe('Adv-1');
 })
 
 
